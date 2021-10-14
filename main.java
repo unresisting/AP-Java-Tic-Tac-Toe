5892f4291bd2782821843b0ddfe87244ea4e434f
Andy <info@aguo.dev>
2021-10-14 14:41:47 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.awt.event.*;
+
+class Main {
+    public static void main(String[] args) {
+		// The main method in here; start a new game.
+		TicTacToe buttonGui= new TicTacToe();
+		buttonGui.setVisible(true);
+	}
+ 
+}
