9caa4c959b5161abb7aedf9450cba8a7aee10e05
Andy <info@aguo.dev>
2021-10-14 14:41:06 -0400

    Create andyPane.java

---
+++ andyPane.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class andyPane extends JFrame{  
+    public andyPane( Color paneColor){
+       this.getContentPane();
+       setBackground(paneColor);
+       setLayout(new BorderLayout());
+    }
+  }
